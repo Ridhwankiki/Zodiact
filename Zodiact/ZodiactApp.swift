ccade6f48d440247448d5c95c12cb537ded01ec3
Ridhwankiki <ridhwanr0607@gmail.com>
2024-05-02 09:25:48 +0700

    Initial Commit

---
+++ Zodiact/ZodiactApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZodiactApp.swift
+//  Zodiact
+//
+//  Created by MacBook Pro on 02/05/24.
+//
+
+import SwiftUI
+
+@main
+struct ZodiactApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
